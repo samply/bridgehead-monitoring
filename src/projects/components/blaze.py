83fdaf6442154c0605a05b66ec5936c11a54cdd7
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-11-15 10:03:34 +0100

    Fixed blaze service

--- src/projects/components/blaze.py
+++ src/projects/components/blaze.py
@@ -4,5 +4,5 @@ from common import Service
 services = [
     Service("Blaze", "blaze-health", "Blaze: Health", "/health", "text", 7200),
     Service("Blaze", "blaze-version", "Blaze: Version", "/fhir/metadata", "json()['software']['version']", 7200),
-    Service("Blaze", "blaze-resources", "Blaze: Verbrauchte Ressourcen", "/fhir", "json()['total']", 7200)
+    ##Service("Blaze", "blaze-resources", "Blaze: Verbrauchte Ressourcen", "/fhir", "json()['total']", 7200)
 ]
