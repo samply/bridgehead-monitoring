ddb3ab3a2dd71721d9088ed72606297402abfdca
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-11-15 10:17:52 +0100

    Working

--- src/projects/components/blaze.py
+++ src/projects/components/blaze.py
@@ -3,6 +3,6 @@ from common import Service
 
 services = [
     Service("Blaze", "blaze-health", "Blaze: Health", "/health", "text", 7200),
-    Service("Blaze", "blaze-version", "Blaze: Version", "/fhir/metadata", "json()['software']['version']", 7200),
+    Service("Blaze", "blaze-version", "Blaze: Version", "/fhir/metadata", "json()['software']['version']", 7200)
     ##Service("Blaze", "blaze-resources", "Blaze: Verbrauchte Ressourcen", "/fhir", "json()['total']", 7200)
 ]
