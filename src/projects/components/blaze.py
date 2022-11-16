1ad6c67d257ce79f30e6ca784bb8712d70cc670e
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-11-16 09:27:34 +0100

    Improve times in blaze

--- src/projects/components/blaze.py
+++ src/projects/components/blaze.py
@@ -2,7 +2,7 @@ from common import Service
 
 
 services = [
-    Service("Blaze", "blaze-health", "Blaze: Health", "/health", "text", 7200),
-    Service("Blaze", "blaze-version", "Blaze: Version", "/fhir/metadata", "json()['software']['version']", 7200)
-    ##Service("Blaze", "blaze-resources", "Blaze: Verbrauchte Ressourcen", "/fhir", "json()['total']", 7200)
+    Service("Blaze", "blaze-health", "Blaze: Health", "/health", "text", 480),
+    Service("Blaze", "blaze-version", "Blaze: Version", "/fhir/metadata", "json()['software']['version']", 480)
+    ##Service("Blaze", "blaze-resources", "Blaze: Ressourcen", "/fhir", "json()['total']", 7200)
 ]
