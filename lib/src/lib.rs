b2144b74c8123679a763ee730e2ed7e6503ccf84
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-08-07 14:58:41 +0000

    `impl Display for Check`

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
@@ -6,4 +8,15 @@ pub enum Check {
     BlazeHealth,
     BlazeVersion,
     BlazeResources,
+}
+
+impl fmt::Display for Check {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match self {
+            Check::BlazeHealth => "Blaze Health",
+            Check::BlazeVersion => "Blaze Version",
+            Check::BlazeResources => "Blaze Resources",
+        };
+        f.write_str(name)
+    }
 }
